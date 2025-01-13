713b5855ae44e4f99d32c04e81cf4e185db149d1
User1 <seveno.e@stjolorient.fr>
2025-01-13 11:47:51 +0100

    correction de encoding

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -1,6 +1,6 @@
 def diff(file_first, file_second):
     """Fonction retournant True si deux fichiers sont différents."""
-    with open(file_first,encoding='utf-8') as f1, open(file_second,encoding='utf-8') as f2:
+    with open(file_first,'r',encoding='utf-8') as f1, open(file_second,'r',encoding='utf-8') as f2:
         return f1.read() != f2.read()
 
 def same(file_first, file_second):
