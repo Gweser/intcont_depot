6f9f9a12f35cd7864d9e50230ac2ac8841644964
User1 <seveno.e@stjolorient.fr>
2025-01-13 11:22:24 +0100

    ajout de la fonction different de

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -1,7 +1,10 @@
-ef diff(file_first, file_second):
+def diff(file_first, file_second):
     """Fonction retournant True si deux fichiers sont différents."""
-    with open(file_first, 'r') as f1, open(file_second, 'r') as f2:
-        return f1.read() != f2.read()
+    resultat = False
+    with open(file_first) as f1:
+		 with open(file_second) as f2:
+			resultat = f1.read() != f2.read()
+	return resultat
 
 def same(file_first, file_second):
     """Fonction retournant True si deux fichiers sont identiques."""
