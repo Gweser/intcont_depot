5fc9ff0cc29ea0dd8656232b6aebe752c5170f72
User1 <seveno.e@stjolorient.fr>
2025-01-13 11:25:00 +0100

    correction de l'indentation

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -1,11 +1,10 @@
 def diff(file_first, file_second):
-    """Fonction retournant True si deux fichiers sont différents."""
-    resultat = False
-    with open(file_first) as f1:
-		 with open(file_second) as f2:
-			resultat = f1.read() != f2.read()
-	return resultat
+  """Fonction retournant True si deux fichiers sont différents."""
+  resultat = False
+  with open(file_first) as f1, open(file_second) as f2:
+    resultat = f1.read() != f2.read()
+  return resultat
 
 def same(file_first, file_second):
-    """Fonction retournant True si deux fichiers sont identiques."""
-    return True
+  """Fonction retournant True si deux fichiers sont identiques."""
+  return True
